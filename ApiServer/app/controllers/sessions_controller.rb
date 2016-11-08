27b0b387da8ec6b2544e28ffd274afff60eed7fa
Armando <aalves.job@gmail.com>
2016-11-07 23:21:42 -0200

    Add on ApiServer return_to after login

--- ApiServer/app/controllers/sessions_controller.rb
+++ ApiServer/app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    session[:return_to] = params[:return_to] if params[:return_to]
   end
 
   def create
@@ -7,7 +8,12 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:success] = "Welcome back!"
-      redirect_to root_path
+      if session[:return_to]
+        redirect_to session[:return_to]
+        session[:return_to] = nil
+      else
+        redirect_to root_path
+      end
     else
       flash[:warning] = "You have entered incorrect email and/or password."
       render :new
--- ApiServer/config/initializers/doorkeeper.rb
+++ ApiServer/config/initializers/doorkeeper.rb
@@ -6,7 +6,7 @@ Doorkeeper.configure do
   resource_owner_authenticator do
     # Put your resource owner authentication logic here.
     # Example implementation:
-    User.find_by_id(session[:user_id]) || redirect_to(new_session_url)
+    User.find_by_id(session[:user_id]) || redirect_to(new_session_url(return_to: request.fullpath))
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
